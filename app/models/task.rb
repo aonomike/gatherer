296bbc239c7c09c8eed3dffef08728bbc75a2ccc
aonomike <aonomike@gmail.com>
2018-12-07 09:30:42 +0300

    The first date implementations

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,15 +2,25 @@ class Task
   attr_accessor :size, :completed
 
   def initialize(options={})
-    @completed = options[:completed]
+
+    mark_completed(options[:completed_at]) if options[:completed_at]
     @size = options[:size]
   end
 
-  def mark_completed
-    @completed = true
+  def mark_completed( date = Time.current )
+    @completed_at = date
   end
 
   def complete?
-    @completed
+    @completed_at.present?
+  end
+
+  def part_of_velocity?
+    return false unless complete?
+    @completed_at > 21.days.ago
+  end
+
+  def points_towards_velocity
+    part_of_velocity? ? @size :0
   end
 end
