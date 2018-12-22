8ee187dd0c7b52da4cee771a582b5d877fd78edf
aonomike <aonomike@gmail.com>
2018-12-22 09:31:23 +0300

    Correct typos on task specs

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -15,19 +15,19 @@ RSpec.describe Task do
     let(:task) { Task.new(size: 3)}
     it "does not count an incomplete task towards velocity" do
       expect(task).not_to be_a_part_of_velocity
-      expect(task.points_towards_velocity).to eq(0)
+      expect(task.points_toward_velocity).to eq(0)
     end
 
     it "counts recently completed tasks towards velocity" do
       task.mark_completed(1.day.ago)
       expect(task).to be_a_part_of_velocity
-      expect(task.points_towards_velocity).to eq(3)
+      expect(task.points_toward_velocity).to eq(3)
     end
 
     it "does not count a long ago completed task toward velocity" do
       task.mark_completed(6.months.ago)
       expect(task).not_to be_a_part_of_velocity
-      expect(task.points_towards_velocity).to eq(0)
+      expect(task.points_toward_velocity).to eq(0)
     end
 
   end
